cd49e482daf00497aa70f1ea49e1641e49fb3a7f
Stefan Muller <smuller2@iit.edu>
2024-03-27 18:05:53 -0500

    Mutex test

---
+++ test/mutex.ml
@@ -0,0 +1,35 @@
+let num_domains = try int_of_string Sys.argv.(1) with _ -> 1
+let t = try int_of_string Sys.argv.(2) with _ -> 43
+let per_thread = 100000
+
+                                               
+module T = Domainslib.Task
+module M = T.Mutex
+
+let mut = M.create ()
+let r = ref 0
+        
+let rec thread pool n =
+  if n <= 0 then Printf.printf "done\n%!"
+  else
+    (M.lock pool mut;
+     Printf.printf "%d\t%d\n%!" (!r) n;
+     r := (!r) + 1;
+     M.unlock pool mut;
+     thread pool (n - 1))
+
+let rec all pool threads =
+  if threads <= 0 then ()
+  else
+    let _ = Printf.printf "all %d\n%!" threads in
+    let a = T.async pool (fun _ -> thread pool per_thread) in
+    all pool (threads - 1);
+    T.await pool a
+
+let main =
+  let pool = T.setup_pool ~num_domains:(num_domains - 1) () in
+  let _ = T.run pool (fun _ -> all pool t) in
+  T.teardown_pool pool;
+  Printf.printf "result = %d\nexpected = %d\n" (!r) (per_thread * t)
+
+let () = main
